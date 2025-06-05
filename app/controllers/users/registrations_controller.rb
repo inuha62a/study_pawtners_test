1fad3b3cb331dc429058e7f38a0dc903c3a14042
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-05 19:02:31 +0900

    パスワードなしでユーザー情報編集

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -39,6 +39,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # protected
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
