7bd37292f6ec212d2f6a79dfe09316fecc0239d5
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-07 15:27:58 +0900

    enum設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ApplicationRecord
+   belongs_to :user
+
+  enum category: {
+    error_log: 0,
+    ai_prompt: 1
+  }
+
+  enum status: {
+    draft: 0,      # 下書き（非公開）
+    published: 1   # 公開中
+  }
 end
