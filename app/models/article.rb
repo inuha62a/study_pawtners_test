0569a90b468a320c5cf8b1c1af502ba365dedd1e
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-07 23:53:56 +0900

    enum記載微調整

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,7 @@
 class Article < ApplicationRecord
    belongs_to :user
 
-  enum category: {
-    error_log: 0,
-    ai_prompt: 1
-  }
-
-  enum status: {
-    draft: 0,      # 下書き（非公開）
-    published: 1   # 公開中
-  }
+   enum category: { error_log: 0, ai_prompt: 1 } # エラー記事 or AIプロンプト
+   enum status: { draft: 0, published: 1 } # 公開・非公開
+ 
 end
