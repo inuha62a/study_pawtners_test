2fb70502ad5961322b45821f961a4d3d282fcfaa
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-10 22:51:55 +0900

    編集・更新・削除機能追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_article, only: [:edit, :update, :destroy]
+  before_action :correct_user!, only: [:edit, :update, :destroy]
 
   def index
     @user = current_user
@@ -23,6 +25,35 @@ class ArticlesController < ApplicationController
       flash.now[:alert] = "記事の作成に失敗しました"
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    # @article は before_action で取得済み
+  end
+  
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: "記事を更新しました"
+    else
+      flash.now[:alert] = "記事の更新に失敗しました"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @article.destroy
+    redirect_to articles_path, notice: "記事を削除しました"
+  end
+
+  # 共通処理をbofore_actionにまとめる
+  # 記事を取得
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # 他人の記事を編集・削除できないようにする
+  def correct_user!
+    redirect_to articles_path, alert: "アクセス権がありません" unless @article.user == current_user
   end
 
   private
