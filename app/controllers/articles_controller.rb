9f1a2c5c09520ad10ad968ed88dadb3d72776889
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-10 21:04:49 +0900

    記事の新規作成機能追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @user = current_user
     @search_form = ArticleSearchForm.new(search_params)
@@ -6,12 +8,31 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to @article, notice: "記事を作成しました"
+    else
+      flash.now[:alert] = "記事の作成に失敗しました"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def search_params
     params.fetch(:article_search_form, {}).permit(:keyword, :category)
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :category, :status, :image)
   end
  
 end
