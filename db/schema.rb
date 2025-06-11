c2b6dd02df75eac6d54a6635586b90be7ec8ff24
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-12 00:10:02 +0900

    StudiesテーブルのDB作成とmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_11_145219) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_11_145838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_11_145219) do
   create_table "comments", force: :cascade do |t|
     t.bigint "article_id", null: false
     t.string "tag", default: [], array: true
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
