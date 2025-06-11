167ef81822cdc43812b8ea9042f5a244cdc6d3e7
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-11 23:55:47 +0900

    Studyモデルの作成とリレーション修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_11_074154) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_11_145219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_11_074154) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "studies", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "date"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_studies_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
@@ -86,4 +95,5 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_11_074154) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
+  add_foreign_key "studies", "users"
 end
