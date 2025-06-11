8ef0cedbf14b2cdeef354c0f68c6a4e4c80b5118
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-11 14:27:42 +0900

    モデルの関連追記

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
    belongs_to :user
+   has_many :comments, dependent: :destroy
 
    enum category: { error_log: 0, ai_prompt: 1 } # エラー記事 or AIプロンプト
    enum status: { draft: 0, published: 1 } # 公開・非公開
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  has_one_attached :image
+
+  enum tag: { official: 0, article_url: 1, ai_prompt: 2, other: 3 }
+
 end
