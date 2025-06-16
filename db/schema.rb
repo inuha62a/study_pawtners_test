57d98d7b2fcff7b74ede073971f19495efb9e575
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-16 15:09:06 +0900

    ContentモデルDB migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_13_084049) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_16_060304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_13_084049) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "contents", force: :cascade do |t|
+    t.bigint "study_id", null: false
+    t.string "name", null: false
+    t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["study_id"], name: "index_contents_on_study_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -98,5 +107,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_13_084049) do
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
+  add_foreign_key "contents", "studies"
   add_foreign_key "studies", "users"
 end
